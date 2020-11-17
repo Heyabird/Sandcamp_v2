420025bdb03b8fc3793b6f0483e496377281f83b
heya <heyabird@gmail.com>
2020-11-16 21:47:58 -0800

    added in artist controller index and show methods and authenticated to rspotify using env variables

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,17 @@
+@client_id = ENV['SPOTIFY_CLIENT_ID']
+@client_secret = ENV['SPOTIFY_CLIENT_SECRET']
+RSpotify::authenticate(@client_id, @client_secret)
+
 class ArtistsController < ApplicationController
   def index
+    if !params[:artist_name].empty?
+      @artists = RSpotify::Artist.search(params[:artist_name])
+    else
+      redirect_to root_path
+    end
   end
 
   def show
+    @artist = RSpotify::Artist.find(params[:id])
   end
 end
