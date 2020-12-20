d99d025c2e0cc94858e20f5612d14e76fda94544
heya <heyabird@gmail.com>
2020-12-20 13:44:20 -0800

    add route for collection

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/artists', to: 'artists#index'
   get '/artists/:id', to:'artists#show', as: 'artist'
+  get '/collection', to: 'collection#index'
 
 end
