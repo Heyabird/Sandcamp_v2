a936bccc950903cf9b556f7a7cae8905b388fa5c
heya <heyabird@gmail.com>
2020-12-23 07:46:05 -0800

    make collections page show existing collections in tiles

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,21 @@
 class CollectionsController < ApplicationController
+    def index
+        @collections = Collection.all.order(created_at: :desc)
+        # render json: @collections
+      end
+    
+      def create
+        @collection = current_user.collections.new(collection_params)
+        respond_to do |format|
+          if @collection.save
+              format.html { redirect_to @collection, notice: 'Album has been saved to collection.' }
+              format.json { render :show, status: :created, location: @collection }
+          else
+              format.html { render :new }
+              format.json { render json: @collection.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end
+
+im stressssssed
