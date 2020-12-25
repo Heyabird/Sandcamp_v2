aff13eeb2037d87197fd9fc217b288812e9cd31a
heya <heyabird@gmail.com>
2020-12-24 19:47:06 -0800

    added new, create, edit, update, and destroy methods to Collections controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,61 +2,63 @@ class CollectionsController < ApplicationController
   # before_action :set_collection, only: [:show, :edit, :update, :destroy]
   # before_action :authenticate_user!
 
-    def index
-      @collections = current_user.collections.order(created_at: :desc)
-        # render json: @collections
-    end
+  def index
+    @collections = current_user.collections.order(created_at: :desc)
+      # render json: @collections
+  end
 
-    def show
-      @collection = current_user.collections.find(params[:id])
-    end
+  def show
+    @collection = Collection.find(params[:id])
+  end
 
-    def new
-      @collection = current_user.Collection.new
-    end
-  
-    def create
-      @collection = current_user.Collection.new(collection_params)
-      respond_to do |format|
-        if @collection.save
-            format.html { redirect_to @collection, notice: 'Album has been saved to collection.' }
-            format.json { render :show, status: :created, location: @collection }
-        else
-            format.html { render :new }
-            format.json { render json: @collection.errors, status: :unprocessable_entity }
-        end
-      end
-    end
+  def new
+    @collection = current_user.Collection.new
+  end
 
-    def edit
-      @collection = current_user.collections.find(params[:id])
-    end
-
-    def update
-      @collection = current_user.collections.find(params[:id])
-      if @collection.update_attributes(collection_params)   
-        flash[:notice] = 'Collection updated!'   
-        redirect_to root_path   
-      else   
-        flash[:error] = 'Failed to edit collection!'   
-        render :edit   
-      end   
-    end   
-
-    def destroy
-      @collection = current_user.collections.find(params[:id])
-      if @collection.delete
-        flash[:notice] = 'Collection deleted'
-        redirect_to root_path
+  def create
+    @collection = current_user.collections.new(collection_params)
+    respond_to do |format|
+      if @collection.save
+          format.html { redirect_to @collection, notice: 'Album has been saved to collection.' }
+          format.json { render :show, status: :created, location: @collection }
       else
-        flash[:error] = 'Failed to delete this collection!'   
-        render :destroy 
-      end  
-    end 
-
-    #use strong parameters for the validation of params
-      def collection_params
-        params.require(:collection).permit(:album_name, :artist_name, :comment)
+          format.html { render :new }
+          format.json { render json: @collection.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def edit
+    @collection = Collection.find(params[:id])
+  end
+
+  def update
+    @collection = Collection.find(params[:id])
+    if @collection.update_attributes(collection_params)   
+      flash[:notice] = 'Collection updated!'   
+      redirect_to root_path   
+    else   
+      flash[:error] = 'Failed to edit collection!'   
+      render :edit   
+    end   
+  end   
+
+  def destroy
+    @collection = Collection.find(params[:id])
+    if @collection.delete
+      flash[:notice] = 'Collection deleted'
+      redirect_to root_path
+    else
+      flash[:error] = 'Failed to delete this collection!'   
+      render :destroy 
+    end  
+  end 
+
+  private
+  
+  #use strong parameters for the validation of params
+  def collection_params
+      params.require(:collection).permit(:album_name, :artist_name, :comment)
+    end
 end
 
