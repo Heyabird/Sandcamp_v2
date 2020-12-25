f2f3e74958e8da7702ad2aa95f1be8be00624b1c
heya <heyabird@gmail.com>
2020-12-24 18:46:51 -0800

    add the rest of CRUD actions in controller and strong params for collection params

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,16 +3,20 @@ class CollectionsController < ApplicationController
   # before_action :authenticate_user!
 
     def index
-      @collections = Collection.all.order(created_at: :desc)
+      @collections = current_user.collections.order(created_at: :desc)
         # render json: @collections
     end
 
     def show
-      @collection = Collection.find(params[:id])
+      @collection = current_user.collections.find(params[:id])
+    end
+
+    def new
+      @collection = current_user.Collection.new
     end
   
     def create
-      @collection = current_user.collections.new(collection_params)
+      @collection = current_user.Collection.new(collection_params)
       respond_to do |format|
         if @collection.save
             format.html { redirect_to @collection, notice: 'Album has been saved to collection.' }
@@ -23,5 +27,36 @@ class CollectionsController < ApplicationController
         end
       end
     end
+
+    def edit
+      @collection = current_user.collections.find(params[:id])
+    end
+
+    def update
+      @collection = current_user.collections.find(params[:id])
+      if @collection.update_attributes(collection_params)   
+        flash[:notice] = 'Collection updated!'   
+        redirect_to root_path   
+      else   
+        flash[:error] = 'Failed to edit collection!'   
+        render :edit   
+      end   
+    end   
+
+    def destroy
+      @collection = current_user.collections.find(params[:id])
+      if @collection.delete
+        flash[:notice] = 'Collection deleted'
+        redirect_to root_path
+      else
+        flash[:error] = 'Failed to delete this collection!'   
+        render :destroy 
+      end  
+    end 
+
+    #use strong parameters for the validation of params
+      def collection_params
+        params.require(:collection).permit(:album_name, :artist_name, :comment)
+      end
 end
 
